6378dd70e138949c4468e1cf0e784da4be30ad8b
“[christian-jurado]” <[christian.jurado.n@gmail.com]>
2025-01-21 17:24:52 -0600

    CSS Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `<h1>{{title}}</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
+  title = 'This is whats is prompting';
 }
